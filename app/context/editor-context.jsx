fc8d98d849ff54725225e5862804c8dea86bc3cd
Nikonus <nikhildubay.2005@gmail.com>
2026-02-26 22:44:50 +0530

    feat: update editor context state management

--- app/context/editor-context.jsx
+++ app/context/editor-context.jsx
@@ -1,11 +1,31 @@
-import { createContext, useContext } from "react";
+"use client";
 
-export const CanvasContext = createContext();
+import { createContext, useContext, useState } from "react";
+
+export const CanvasContext = createContext(null);
+
+export const CanvasProvider = ({ children }) => {
+  const [canvasEditor, setCanvasEditor] = useState(null);
+  const [activeTool, setActiveTool] = useState(null);
+
+  return (
+    <CanvasContext.Provider
+      value={{
+        canvasEditor,
+        setCanvasEditor,
+        activeTool,
+        setActiveTool,
+      }}
+    >
+      {children}
+    </CanvasContext.Provider>
+  );
+};
 
 export const useCanvas = () => {
   const context = useContext(CanvasContext);
   if (!context) {
-    throw new Error("Error");
+    throw new Error("useCanvas must be used inside CanvasProvider");
   }
   return context;
 };
